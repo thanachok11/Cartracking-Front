8f392112723e7ba9f23674125c32febcf2c70a02
Thanachok Suwan <119285237+thanachok11@users.noreply.github.com>
2025-09-02 14:04:36 +0700

    Update DataTodayPage.tsx

--- src/pages/DataTodayPage.tsx
+++ src/pages/DataTodayPage.tsx
@@ -429,7 +429,7 @@ export default function DataTodayPage() {
                 </select>
               </div>
               <div className="row">
-                <label>สถานะ</label>
+                <label>ตำแหน่ง</label>
                 <input value={form.station_in || ''} onChange={(e) => handleChange('station_in', e.target.value)} required />
               </div>
               <div className="row">
@@ -454,7 +454,7 @@ export default function DataTodayPage() {
               <th>ทะเบียนหัว</th>
               <th>ทะเบียนหาง</th>
               <th>หมายเลขตู้</th>
-              <th>สถานะ</th>
+              <th>ตำแหน่ง</th>
               <th>บริษัท</th>
               <th></th>
             </tr>
